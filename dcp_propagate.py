95fd357bdbf0d47b826040a7381e1228d39a6472
Donald Paul Smith aka FatherTimeSDKP or father time https://fathertimesdkp.github.io <dallasnamiyadaddy@gmail.com>
2025-10-16 07:53:14 -0400

    Update dcp_propagate.py

--- dcp_propagate.py
+++ dcp_propagate.py
@@ -1,3 +1,4 @@
+https://github.com/FatherTimeSDKP/Peer-review-failed-me.-
 python dcp_propagate.py
 git add .
 git commit -m "DCP Deployment FINALIZED: Embedded permanent Authorship Ledger and official metadata files. Protocol fully active."
